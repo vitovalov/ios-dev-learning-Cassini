2f7d77dff32d8a056b92d16c7409593dd4a5f223
Vito Valov <vitovalov@gmail.com>
2017-05-24 13:03:18 +0200

    fetching image badly

--- Cassini/ImageViewController.swift
+++ Cassini/ImageViewController.swift
@@ -11,13 +11,33 @@ import UIKit
 /// VC that shows an image
 class ImageViewController: UIViewController {
 
-    var imageURL: URL?
+    var imageURL: URL? {
+        didSet {
+            image = nil
+            fetchImage() // since it might be internet image
+        }
+    }
+    
+    private func fetchImage() {
+        if let url = imageURL {
+            let urlContents = try? Data(contentsOf: url)
+            if let imageData = urlContents {
+                image = UIImage(data: imageData)
+            }
+        }
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.addSubview(imageView)
+        imageURL = DemoURL.stanford
+    }
     
     private var imageView = UIImageView()
     
     private var image: UIImage? {
         get {
-            imageView.image
+            return imageView.image
         }
         
         set {
